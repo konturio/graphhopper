cf65bb7882315db709dca1ac9e22d7bd710af4be
easbar <easbar.mail@posteo.net>
2019-03-28 07:39:35 +0100

    Adds test showing read speed in wrong direction error.
    Signed-off-by: easbar <easbar.mail@posteo.net>

--- core/src/test/java/com/graphhopper/routing/RandomCHRoutingTest.java
+++ core/src/test/java/com/graphhopper/routing/RandomCHRoutingTest.java
@@ -61,15 +61,12 @@ public class RandomCHRoutingTest {
         int numNodes = 50;
         long seed = System.nanoTime();
         // for example these used to fail before fixing #1574.
-//        seed = 9348906923700L;
-//        seed = 9376976930825L;
-//        seed = 9436934744695L;
-//        seed = 10093639220394L;
+        seed = 10785899964423L;
 
         System.out.println("seed: " + seed);
         Random rnd = new Random(seed);
-        // todo: allowing loops also produces another error (may not read speed in wrong direction...), e.g. with this seed: 10785899964423
-        boolean allowLoops = false;
+        // todonow: allowing loops here  produces error (may not read speed in wrong direction...), fix this!
+        boolean allowLoops = true;
         buildRandomGraph(rnd, numNodes, 2.5, allowLoops, true, 0.9);
         locationIndex = new LocationIndexTree(graph, dir);
         locationIndex.prepareIndex();
